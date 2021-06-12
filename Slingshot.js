f63ccb8e59318ec566166c2a29afa736ccb896a3
Joel-make <iamjoeljoseph21@gmail.com>
2021-06-12 15:35:01 +0530

    Added catapult

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1= loadImage("sprites/sling1.png");
+        this.sling2= loadImage("sprites/sling2.png");
+        this.sling3= loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -19,9 +22,27 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x < 200)
+            {
+            strokeWeight(20);
+            stroke("#301608");
+            line(pointA.x - 20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x -20 , pointA.y, pointB.x + 30, pointB.y);
+            image(this.sling3,pointA.x-30 ,pointA.y-15,15,30);
+            }
+            else {
+            strokeWeight(12);
+            stroke("#301608");
+            line(pointA.x + 20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x +20 , pointA.y, pointB.x + 30, pointB.y);
+            image(this.sling3,pointA.x+20 ,pointA.y-15,15,30);
+            }
+
         }
+        image(this.sling1,200,20);
+        image(this.sling2,175,20);
+
+
     }
     
 }
